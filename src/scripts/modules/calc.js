62db78d586ebcd4a98e08011e2fef987f44abcf4
yuriybevov <yuriybevov@gmail.com>
2024-03-20 21:15:59 +0300

    add calc js

--- src/scripts/modules/calc.js
+++ src/scripts/modules/calc.js
@@ -1,35 +1,76 @@
-const calc = document.querySelector('.calc');
+const calc = document.querySelector(".calc");
 
-if(calc) {
-    const ctrls = calc.querySelectorAll('[data-price], [data-multiplier]');
-    const total = calc.querySelector('.calc__total-price span');
-    const basePrice = calc.querySelector('[data-base-price]');
+if (calc) {
+  const ctrls = calc.querySelectorAll("[data-price], [data-multiplier]");
+  const total = calc.querySelector(".calc__total-price span");
+  const basePrice = calc.querySelector("[data-base-price]");
+  const multiplierValue = Number(
+    calc.querySelector("[data-multiplier]").dataset.multiplier
+  );
 
-    let currentValue = Number(basePrice.dataset.basePrice.replaceAll(' ', ''));
+  const delivery = calc.querySelector(
+    ".calc__delivery-field--main input[type='checkbox']"
+  );
+  const regions = calc.querySelector(".calc__delivery-field--radio");
 
-    console.log('BASE PRICE:', basePrice, currentValue);
+  let currentValue = Number(basePrice.dataset.basePrice.replaceAll(" ", ""));
 
+  const numberPrettify = (number) => {
+    const separator = " ";
+    return number
+      .toString()
+      .replace(/(\d{1,3}(?=(?:\d\d\d)+(?!\d)))/g, "$1" + separator);
+  };
 
+  const fillPriceNode = (value) => {
+    total.innerHTML = numberPrettify(Math.round(value));
+  };
 
-    const numberPrettify = (number) => {
-        const separator = " ";
-        return number.toString().replace(/(\d{1,3}(?=(?:\d\d\d)+(?!\d)))/g, "$1" + separator);
-    }
+  fillPriceNode(currentValue);
 
-    const fillPriceNode = (value) => {
-        total.innerHTML = numberPrettify(value);
-    }
+  const setTotalPrice = () => {
+    let total = isMultiplier ? currentValue * multiplierValue : currentValue;
 
-    fillPriceNode(currentValue);
-
-    ctrls.forEach(ctrl => {
-        ctrl.addEventListener('click', () => {
-            const price = Number(ctrl.dataset.price.replaceAll(' ', ''));
-            const multiplier = Number(ctrl.dataset.multiplier.replaceAll(' ', ''));
-            currentValue += price * multiplier;
-            
-            fillPriceNode(currentValue);
-        })
+    ctrls.forEach((ctrl) => {
+      if (ctrl.checked && ctrl.dataset.price) {
+        total += Number(ctrl.dataset.currentPrice)
+          ? Number(ctrl.dataset.currentPrice)
+          : Number(ctrl.dataset.price);
+      }
     });
 
+    fillPriceNode(total);
+  };
+
+  let isMultiplier = false;
+
+  const setMultiplier = () => {
+    isMultiplier = !isMultiplier;
+
+    ctrls.forEach((ctrl) => {
+      ctrl.dataset.currentPrice = isMultiplier
+        ? ctrl.dataset.price * multiplierValue
+        : ctrl.dataset.price;
+    });
+
+    setTotalPrice();
+  };
+
+  ctrls.forEach((ctrl) => {
+    ctrl.addEventListener("click", () => {
+      if (ctrl.dataset.price) {
+        setTotalPrice();
+      }
+
+      if (ctrl.dataset.multiplier) {
+        setMultiplier();
+      }
+    });
+  });
+
+  delivery.addEventListener("change", () => {
+    delivery.checked
+      ? regions.classList.remove("disabled")
+      : regions.classList.add("disabled");
+  });
 }
